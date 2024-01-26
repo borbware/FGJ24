3a85aca1924ed9b8faf0d24b3c858fb72cded606
borb <borbware@gmail.com>
2024-01-26 22:42:44 +0200

    something

--- scenes/player_movement.gd
+++ scenes/player_movement.gd
@@ -1,16 +1,25 @@
 extends Node3D
 
+var arm1 = null
+var arm2 = null
+@export var rotation_strength: float
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	arm1 = get_node("Arm1")
+	arm2 = get_node("Arm2")
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var left_stick = Vector2(
 		Input.get_axis("left_stick_left", "left_stick_right"),
 		Input.get_axis("left_stick_up", "left_stick_down")
 	)
+	var right_stick = Vector2(
+		Input.get_axis("right_stick_left", "right_stick_right"),
+		Input.get_axis("right_stick_up", "right_stick_down")
+	)
+	print(rotation_strength)
 	if left_stick:
-		print(left_stick)
+		arm1.rotation = Vector3(0, left_stick.x, left_stick.y) * rotation_strength;
+	if right_stick:
+		arm1.rotation = Vector3(0, right_stick.x, right_stick.y) * rotation_strength;
