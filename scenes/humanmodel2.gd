288bb9801fb00ddab2f063205b0747b84f4ed145
borb <borbware@gmail.com>
2024-01-27 19:18:46 +0200

    add tykki, ammo and stuff

--- scenes/humanmodel2.gd
+++ scenes/humanmodel2.gd
@@ -4,21 +4,49 @@ var LeftHandTarget
 var RightHandTarget
 var LeftShoulder
 var RightShoulder
+var LeftHand
+var RightHand
+var IKleft
+var IKright
 var left_stick : Vector2
 var right_stick : Vector2
 var size = 1
+enum {
+	LOOK_FOR_HANDLE,
+	HOLD_TO_HANDLE
+}
+enum {
+	LEFT,
+	RIGHT
+}
+var left_mode = LOOK_FOR_HANDLE
+var right_mode = LOOK_FOR_HANDLE
+
+var DebugHoldLeft
 
 func _ready():
 	var skeleton = $ego/ego_rig/Skeleton3D
 	skeleton.physical_bones_start_simulation()
-	var IKleft = skeleton.get_node("SkeletonIK3DLeft")
-	var IKright = skeleton.get_node("SkeletonIK3DRight")
+	IKleft = skeleton.get_node("SkeletonIK3DLeft")
+	IKright = skeleton.get_node("SkeletonIK3DRight")
 	LeftShoulder = skeleton.get_node("Physical Bone DEF-shoulder_L")
 	RightShoulder = skeleton.get_node("Physical Bone DEF-shoulder_R")
+	LeftHand = skeleton.get_node("Physical Bone DEF-forearm_L_001")
+	RightHand = skeleton.get_node("Physical Bone DEF-forearm_R_001")
 	LeftHandTarget = get_parent().get_node("LeftHandTarget")
 	RightHandTarget = get_parent().get_node("RightHandTarget")
-	IKleft.start()
-	IKright.start()
+	DebugHoldLeft = get_parent().get_node("LeftHold")
+	# IKleft.start()
+	# IKright.start()
+	# set_target_to_hold(LEFT, DebugHoldLeft)
+
+func set_target_to_hold(side, target):
+	var IK = IKleft if side == LEFT else IKright  
+	IK.target_node = target
+	if side == LEFT:
+		left_mode = HOLD_TO_HANDLE
+	elif side == RIGHT:
+		right_mode = HOLD_TO_HANDLE
 
 func _process(delta):
 	left_stick = Vector2(
@@ -34,5 +62,6 @@ func _process(delta):
 
 
 func _physics_process(delta):
-	# get_node("ego/ego_rig/Skeleton3D/Physical Bone DEF-forearm_L").position = Vector3(-1,2,4)
 	pass
+	# LeftHand.global_position = DebugHoldLeft.global_position
+	# print(LeftHand.global_position)
