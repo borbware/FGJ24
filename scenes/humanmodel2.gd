e8e3a982c0b38bcbb031f0c1063b38831241fa6b
borb <borbware@gmail.com>
2024-01-27 14:41:46 +0200

    make legs more like legs

--- scenes/humanmodel2.gd
+++ scenes/humanmodel2.gd
@@ -5,5 +5,6 @@ func _ready():
 	get_node("ego/ego_rig/Skeleton3D").physical_bones_start_simulation()
 
 
-func _process(delta):
+func _physics_process(delta):
 	pass
+	# get_node("ego/ego_rig/Skeleton3D/Physical Bone DEF-forearm_L").position = Vector3(0,0,0)
