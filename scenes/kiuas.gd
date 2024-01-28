7e4b2fb7f244775e44f0588ca366e2e76e820def
borb <borbware@gmail.com>
2024-01-28 14:27:28 +0200

    add reset button

--- scenes/kiuas.gd
+++ scenes/kiuas.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var win = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,10 +25,11 @@ func _on_area_3d_area_shape_entered(area_rid, area, area_shape_index, local_shap
 func _on_area_3d_body_entered(body):
 	var object = body.get_parent()
 	print(body)
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and win == false:
+		win = true
 		get_node("AudioStreamPlayer").play()
 		await get_tree().create_timer(1).timeout
-		get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+		get_tree().change_scene_to_file("res://scenes/win.tscn")
 	elif body.is_in_group("Ammo"):
 		body.queue_free()
 
